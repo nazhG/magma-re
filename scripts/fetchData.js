0ea9171608d32dcdf36e4741d44844efaa4ecb6c
nazhG <nazhirgonzalez@gmail.com>
2022-10-07 14:46:09 -0400

    primer muestra

--- scripts/fetchData.js
+++ scripts/fetchData.js
@@ -3,6 +3,7 @@ import Web3 from "web3";
 import EthDater from "ethereum-block-by-date";
 import CHAINLINK_ORACLE from "../src/abis/CHAINLINK.js";
 import SAND from "../src/abis/SAND.js";
+import PAIR_ABI from "../src/abis/pair_uniswap.js";
 
 const web3 = new Web3(
   // "https://greatest-falling-dinghy.discover.quiknode.pro/d471dd0a80b083394e8f01131feef4537db4cb99"
@@ -14,25 +15,25 @@ const dater = new EthDater(web3);
 const date_start = new Date();
 const date_end = date_start.getDate() - 7;
 let blocks = await dater.getEvery(
-  "hours",
-  "2022-09-25T00:00:00Z",
-  "2022-09-30T00:00:00Z",
+  "days",
+  "2021-10-07T00:00:00Z",
+  "2022-10-07T00:00:00Z",
   1
 );
+
+const PAIR_SAND_USDC = "0x5864DEa5f1750D1f8887F9FB7f3a50F15789514E";
+const pair_sand_usdc = new web3.eth.Contract(PAIR_ABI, PAIR_SAND_USDC);
 
 const Advisors = "0x07d2601739709C25FE0AfD50EC961BA589311CaA";
 const Company = "0x2F2456953A2cBc21fdE058D33fb2aD8D59E72A35";
 const Team = "0x5f5B8942aE6325227d5d82e5759E837271Fa2a67";
 const Foundation = "0x8FFA64FB50559c3Ff09a1022b84B2c5233ed8068";
 
+const pair = new web3.eth.Contract(PAIR_ABI, "0x1b40183efb4dd766f11bda7a7c3ad8982e998421");
+
 const Sand = new web3.eth.Contract(
   SAND,
   "0x3845badAde8e6dFF049820680d1F14bD3903a5d0"
-);
-
-const priceFeed = new web3.eth.Contract(
-  CHAINLINK_ORACLE,
-  "0x35E3f7E558C04cE7eEE1629258EcbbA03B36Ec56"
 );
 
 const sleep = (milliseconds) => {
@@ -43,15 +44,19 @@ const sleep = (milliseconds) => {
 
 const write = async () => {
   let content = "export default [\n";
+  let content2 = "export default [\n";
   const totalSupply = new BN(await Sand.methods.totalSupply().call());
 
   for (const i of blocks) {
     let blockNumberTarget = i.block;
     console.log(i, "of ", blocks.length);
-    
-    let { answer: price } = await priceFeed.methods
-      .latestRoundData()
-      .call({}, blockNumberTarget);
+
+    const { sqrtPriceX96 } = await pair_sand_usdc.methods.slot0().call({}, blockNumberTarget);
+    const sqrtPriceinUsdc = new BN(sqrtPriceX96);
+    const sqrtPrice = sqrtPriceinUsdc.mul(sqrtPriceinUsdc);
+    const base = new BN(2).pow(new BN(192)).div(new BN(1e6));
+
+    const price = sqrtPrice.mul(new BN(1e8)).div(base);
 
     const AdvisorsBalance = new BN(
       await Sand.methods.balanceOf(Advisors).call({}, blockNumberTarget)
@@ -73,22 +78,28 @@ const write = async () => {
       .sub(CompanyBalance)
       .sub(TeamBalance)
       .sub(FoundationBalance)
-      .mul(new BN(price));
+      .mul(new BN(price))
+      .div(new BN(1e14))
+      .div(new BN(1e6));
+
+     const pricesFormated = Number(price)/100;
 
     content += `["${i.date.toString()}",${LiquidSupply.toString()}],\n`;
+    content2 += `["${i.date.toString()}",${pricesFormated.toString()}],\n`;
 
-    // console.log(
-    //   "blockNumberTarget",
-    //   blockNumberTarget,
-    //   "price",
-    //   price,
-    //   "LiquidSupply",
-    //   LiquidSupply.toString()
-    // );
+    console.log(
+      "blockNumberTarget",
+      blockNumberTarget,
+      "price",
+      price.toString(),
+      "LiquidSupply",
+      LiquidSupply.toString()
+    );
   }
   content += "];";
+  content2 += "];";
   fs.writeFileSync("sand_data_09_29.js", content);
+  fs.writeFileSync("sand_price_09_29.js", content2);
 };
-console.log(Date());
+
 write();
-console.log(Date());
--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,53 @@ import CHAINLINK_ORACLE from "./abis/CHAINLINK.js";
 import months from "./abis/months";
 import { Chart } from "react-google-charts";
 import SAND_DATA from "../sand_data_09_29.js";
+import SAND_PRICE from "../sand_price_09_29.js";
+import { FaTwitter, FaDiscord, FaEllipsisV } from "react-icons/fa";
 
 const options = {
-  title: "Company Performance",
+  title: "Price (USD)",
   curveType: "function",
-  legend: { position: "bottom" },
+  titleTextStyle: {
+    color: "#fff",
+  },
+  legend: {
+    textStyle: {
+      color: "#fff",
+    },
+  },
+  backgroundColor: "#1a1a1a",
   hAxis: {
-    ticks: ["$8.00", "$6.00", "$4.00", "$2.00", "$0.00"],
+    textStyle: {
+      color: "#fff",
+    },
+  },
+  vAxis: {
+    textStyle: {
+      color: "#fff",
+    },
+  },
+};
+
+const options2 = {
+  title: "Marked Cap (USD)",
+  titleTextStyle: {
+    color: "#fff",
+  },
+  legend: {
+    textStyle: {
+      color: "#fff",
+    },
+  },
+  backgroundColor: "#1a1a1a",
+  hAxis: {
+    textStyle: {
+      color: "#fff",
+    },
+  },
+  vAxis: {
+    textStyle: {
+      color: "#fff",
+    },
   },
 };
 
@@ -28,29 +68,324 @@ function App() {
   const [currentBlockNumber, setcurrentBlockNumber] = useState(0);
   const [currentTime, setcurrentTime] = useState(0);
   const [data, setData] = useState([["Month", "Liquid Supply"]]);
+  const [price, setPrice] = useState([["Month", "Price"]]);
+
+  let dollarUS = Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
 
   useEffect(() => {
     setData([...data, ...SAND_DATA]);
+    setPrice([...price, ...SAND_PRICE]);
   }, []);
   return (
     <>
-      <h1>
-        Magma-Re / current block num: {currentBlockNumber} - time: {currentTime}{" "}
-      </h1>
+      <header className="container-fuild">
+        <div className="row p-3">
+          <span className="col-9">
+            <img src="./logo.png" alt="logo" />
+          </span>
+          <span className="col-3 d-flex justify-content-between">
+            <a
+              className="text-secondary"
+              href="https://twitter.com/0xSand"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaTwitter />
+            </a>
+            <a
+              className="text-secondary"
+              href="https://discord.gg/0xSand"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaDiscord />
+            </a>
+            <FaEllipsisV />
+          </span>
+        </div>
+      </header>
 
-      {/* <button onClick={getSandBalance}>
-        Get Sand Liquity Balance: {liquityBalance}
-      </button> */}
-      <hr />
-
-      <Chart
-        chartType="LineChart"
-        data={data}
-        options={options}
-        width="80%"
-        height="400px"
-        legendToggle
-      />
+      <div className="container-fuild">
+        <div className="row mx-2">
+          <div className="col-3 logo-verse-container marco p-0">
+            <img
+              className="float-start logo-verse "
+              src="https://static.nftgo.io/asset/1624853691744.png"
+              alt=""
+            />
+          </div>
+          <div className="col-9 p-0 ps-3">
+            <div className="marco p-3 h-100">
+              <h1>The Sandbox</h1>
+              <p className="text-muted">
+                "A Decentralized Gaming Ecosystem Made By Players"
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container marco mt-4">
+        <div className="flex-row">
+          <nav className="navbar col-12">
+            <a className="nav-link" href="#overview">
+              Overview
+            </a>
+            <a className="nav-link" href="#tokenomics">
+              Metics
+            </a>
+            <a className="nav-link" href="#roadmap">
+              Specs
+            </a>
+            <a className="nav-link" href="#team">
+              Lands
+            </a>
+            <a className="nav-link" href="#partners">
+              Asset
+            </a>
+            <a className="nav-link" href="#contact">
+              Avatars
+            </a>
+          </nav>
+        </div>
+        <hr className="row" />
+        <div className="row">
+          <div className="col-6">
+            <h3 className="bg-dark rounded p-2">Native Coin</h3>
+            <table className="table table-dark">
+              <tbody>
+                <tr>
+                  <th scope="row">Coin Name</th>
+                  <td className="dato">SAND</td>
+                </tr>
+                <tr>
+                  <th scope="row">Coin Value</th>
+                  <td className="dato">
+                    {dollarUS.format(SAND_PRICE[SAND_PRICE.length - 1][1])}
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">Market Cap</th>
+                  <td className="dato">
+                    {dollarUS.format(
+                      Number(SAND_DATA[SAND_DATA.length - 1][1])
+                    )}
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">Daily Volume</th>
+                  <td className="dato">--</td>
+                </tr>
+                <tr>
+                  <th scope="row">Circulating Supply</th>
+                  <td className="dato">
+                    {numberWithCommas(
+                      (
+                        SAND_DATA[SAND_DATA.length - 1][1] /
+                        SAND_PRICE[SAND_PRICE.length - 1][1]
+                      ).toFixed()
+                    )}
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">Max Supply</th>
+                  <td className="dato">3,000,000,000</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="col-6">
+            <h3 className="bg-dark rounded p-2">LAND NFTs</h3>
+            <p>
+              Token Name: <span className="dato">$Sand</span>
+            </p>
+            <p>
+              Floor Price: <span className="dato">$0.93</span>
+            </p>
+            <p>
+              Market Cap: <span className="dato">$0.93</span>
+            </p>
+            <p>
+              Daily Volume: <span className="dato">$0.93</span>
+            </p>
+            <p>
+              Holdes: <span className="dato">$0.93</span>
+            </p>
+            <p>
+              Circulating: <span className="dato">$0.93</span>
+            </p>
+            <p>
+              Max Supply: <span className="dato">$0.93</span>
+            </p>
+          </div>
+          <div className="col-6">
+            <h3 className="bg-dark rounded p-2">ASSETS / WEARABLES NFTs</h3>
+            <p>
+              Token Name: <span className="dato">$Sand</span>
+            </p>
+            <p>
+              Floor Price: <span className="dato">$0.93</span>
+            </p>
+            <p>
+              Market Cap: <span className="dato">$0.93</span>
+            </p>
+            <p>
+              Daily Volume: <span className="dato">$0.93</span>
+            </p>
+            <p>
+              Holdes: <span className="dato">$0.93</span>
+            </p>
+            <p>
+              Circulating: <span className="dato">$0.93</span>
+            </p>
+            <p>
+              Max Supply: <span className="dato">$0.93</span>
+            </p>
+          </div>
+          <div className="col-6">
+            <h3 className="bg-dark rounded p-2">Additional NFT Features</h3>
+            <p>
+              NFT Name: <span className="dato">$Sand</span>
+            </p>
+            <p>
+              Floor Price: <span className="dato">$0.93</span>
+            </p>
+            <p>
+              Market Cap: <span className="dato">$0.93</span>
+            </p>
+            <p>
+              Daily Volume: <span className="dato">$0.93</span>
+            </p>
+            <p>
+              Holdes: <span className="dato">$0.93</span>
+            </p>
+            <p>
+              Circulating: <span className="dato">$0.93</span>
+            </p>
+            <p>
+              Max Supply: <span className="dato">$0.93</span>
+            </p>
+          </div>
+          <div className="col-12">
+            <h3 className="bg-dark rounded p-2">Community Network</h3>
+            <div className="d-flex justify-content-between">
+              <span>
+                <a
+                  className="btn btn-secondary discord"
+                  href="https://discord.com/invite/thesandboxgame"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Discord
+                </a>
+              </span>
+              <span>
+                <a
+                  className="btn btn-secondary twitter"
+                  href="https://twitter.com/thesandboxgame"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Twitter
+                </a>
+              </span>
+              <span>
+                <a
+                  className="btn btn-secondary twitch"
+                  href="https://www.twitch.tv/thesandboxgame"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Twitch
+                </a>
+              </span>
+              {/* <span>
+                <a
+                  className="btn btn-secondary telegram"
+                  href="https://t.me/sandboxgame"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Telegram
+                </a>
+              </span>
+              <span>
+                <a
+                  className="btn btn-secondary reddit"
+                  href="https://www.reddit.com/r/TheSandboxGaming/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Reddit
+                </a>
+              </span> */}
+              <span>
+                <a
+                  className="btn btn-secondary instagram"
+                  href="https://www.instagram.com/thesandboxgame"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Instagram
+                </a>
+              </span>
+              <span>
+                <a
+                  className="btn btn-secondary facebook"
+                  href="https://www.facebook.com/TheSandboxGame/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Facebook
+                </a>
+              </span>
+              <span>
+                <a
+                  className="btn btn-secondary youtube"
+                  href="https://www.youtube.com/channel/UCzv1t7voB-bxMmXLysT4h0w"
+                >
+                  Youtube
+                </a>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="row mt-3">
+          <h3 className="bg-dark rounded p-2 mx-2">Metaverse Performance</h3>
+          <div className="col-10 offset-1">
+            <div className="container marco bg-secondary p-3 display m-4">
+              <Chart
+                className="m-0"
+                chartType="LineChart"
+                data={data}
+                options={options2}
+                width="100%"
+                height="300px"
+                legendToggle
+              />
+            </div>
+          </div>
+          <div className="col-10 offset-1">
+            <div className="container marco bg-secondary p-3 display m-4">
+              <Chart
+                className="m-0"
+                chartType="LineChart"
+                data={price}
+                options={options}
+                width="100%"
+                height="300px"
+                legendToggle
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
