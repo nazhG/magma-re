20ed32c7d26e6a07710bbd7567abfbdff96c868d
nazhG <nazhirgonzalez@gmail.com>
2022-11-02 12:53:58 -0400

    meta

---
+++ src/metaverses.js
@@ -0,0 +1,163 @@
+// TODO: add metamask tokens
+// TODO: check links by code
+export const metaverses = [
+  {
+    name: "Sandbox",
+    token: "SAND",
+    address: "0x3845badAde8e6dFF049820680d1F14bD3903a5d0",
+    image: "https://static.nftgo.io/asset/1624853691744.png",
+    link: "https://sandbox.game/",
+    tittle: "A Decentralized Gaming Ecosystem Made By Players",
+    description:
+      "The Sandbox is a community-driven platform where creators can monetize voxel assets and gaming experiences on the blockchain. The Sandbox metaverse comprises a map made up of 166,464 LANDS. LAND owners can host contests and events, stake SAND to earn and customize assets, monetize assets and experiences, vote in the metaverse governance, play games that you or others create, and more! Trade the collection and keep your eyes peeled for future drops",
+    whitepaper: "https://installers.sandbox.game/The_Sandbox_Whitepaper_2020.pdf",
+    sites: ["https://sandbox.game/", "https://medium.com/sandbox-game"],
+    socials: [
+      { twitter: "https://twitter.com/TheSandboxGame" },
+      { discord: "https://discord.com/invite/vAe4zvY" },
+      { telegram: "https://t.me/sandboxgame" },
+      { reddit: "https://www.reddit.com/r/TheSandboxGaming/" },
+      { instagram: "https://www.instagram.com/thesandboxgame" },
+      { youtube: "https://www.youtube.com/c/TheSandboxGame" },
+      { facebook: "https://www.facebook.com/TheSandboxGame/" },
+    ],
+    // privileged, address of the project participants, which are deducted from the calculations of the available balance.
+    privileged: [
+      {
+        name: "Advisors",
+        address: "0x3845badade8e6dff049820680d1f14bd3903a5d0",
+      },
+      {
+        name: "Company Reserve",
+        address: "0x3845badade8e6dff049820680d1f14bd3903a5d0",
+      },
+      {
+        name: "Team",
+        address: "0x3845badade8e6dff049820680d1f14bd3903a5d0",
+      },
+      {
+        name: "Foundation",
+        address: "0x3845badade8e6dff049820680d1f14bd3903a5d0",
+      },
+    ],
+  },
+  {
+    name: "Decentraland",
+    token: "MANA",
+    address: "0x0f5d2fb29fb7d3cfee444a200298f468908cc942",
+    image: "https://static.nftgo.io/asset/1626416314237.png",
+    link: "https://decentraland.org/",
+    tittle:
+      "Create, explore and trade in the first-ever virtual world owned by its users.",
+    description: "",
+    whitepaper: "https://decentraland.org/whitepaper.pdf",
+    sites: ["https://decentraland.org/", "https://snapshot.org/#/snapshot.dcl.eth", "https://bitcointalk.org/index.php?topic=1968414.0"],
+    socials: [
+        { twitter: "https://twitter.com/decentraland" },
+        { discord: "https://discord.com/invite/da2jFcf" },
+        { telegram: "" },
+        { reddit: "https://reddit.com/r/decentraland" },
+        { instagram: "https://www.instagram.com/decentraland_foundation" },
+        { youtube: "https://www.youtube.com/channel/UCLXcTgzr05cFZaN972nuEXw" },
+        { facebook: "" },
+      ],
+    privileged: [
+      {
+        name: "DAO Vesting Contract",
+        address: "0x7a3abf8897f31b56f09c6f69d074a393a905c1ac",
+      },
+      {
+        name: "Foundation",
+        address: "0xa3a90cacfd83ea4b114d5d7a24b54b42f670af94",
+      },
+      {
+        name: "Decentraland: DAO Agent",
+        address: "0x9a6ebe7e2a7722f8200d0ffb63a1f6406a0d7dce",
+      },
+    ],
+  },
+  {
+    name: "Somnium Space",
+    token: "CUBE",
+    address: "0xdf801468a808a32656d2ed2d2d80b72a129739f4",
+    image:
+      "https://lh3.googleusercontent.com/mzUNo5vk95qQfpAbXir0_6oJmZlyqnq_ix3BIjmfeVGrFPoxeAqf-vYHMvh115bSdJGxRtgGTWKldOzdJQGtEqGW=s130",
+    link: "https://somniumspace.com/",
+    tittle:
+      "SOMNIUM SPACE IS A VIRTUAL REALITY WORLD: Open, Social & Persistent.",
+    description:
+      "Somnium Space is a virtual world where you can create, play, and monetize your content. It’s a place where you can be anyone you want to be, and do anything you want to do. It’s a place where you can be anyone you want to be, and do anything you want to do.",
+    whitepaper: "https://somniumspace.com/files/Somnium%20Space%20Economy%20Paper.pdf",
+    sites: [
+      "https://www.somniumspace.com/",
+      "https://medium.com/@SomniumSpace",
+      "https://www.linkedin.com/company/somnium-space/",
+    ],
+    socials: [
+      { twitter: "https://twitter.com/SomniumSpace" },
+      { discord: "https://discord.com/invite/somniumspace" },
+      { telegram: "https://t.me/somniumspace" },
+      { reddit: "https://www.reddit.com/r/SomniumSpaceVR/" },
+      { instagram: "https://www.instagram.com/somniumspace/" },
+      { youtube: "https://www.youtube.com/c/SomniumSpaceVR" },
+      { facebook: "https://www.facebook.com/SomniumSpace" },
+    ],
+    // privileged, address of the project participants, which are deducted from the calculations of the available balance.
+    privileged: [
+      {
+        name: "developers",
+        address: "0xb98cdacd006b9d47c37ca63cc86f916ee23fc550",
+      },
+    ],
+  },
+  // poligon
+  {
+    name: "Voxels",
+    token: "VOXEL",
+    address: "0xd0258a3fd00f38aa8090dfee343f10a9d4d30d3f",
+    image: "https://www.voxels.com/images/Voxels_logo_128.png",
+    link: "https://voxies.io/",
+    tittle:
+      "A USER-OWNED VIRTUAL WORLD: Players can buy land, build on it, customize their avatar with wearable NFTs and more!",
+    description: "No description.",
+    whitepaper: "https://whitepaper.voxies.io/tokenomics/voxel-token",
+    sites: [
+      "https://voxiesnft.medium.com/",
+    ],
+    socials: [
+      { twitter: "https://twitter.com/VoxiesNFT" },
+      { discord: "https://discord.com/invite/eQzGhVJ8F9" },
+      { telegram: "" },
+      { reddit: "https://www.reddit.com/r/VoxiesNFT/" },
+      { instagram: "" },
+      { youtube: "" },
+      { facebook: "https://www.facebook.com/VoxiesNFT/" },
+    ],
+  },
+  {
+    name: "Otherside",
+    token: "$APE",
+    address: "0x4d224452801aced8b2f0aebe155379bb5d594381",
+    image:
+      "https://lh3.googleusercontent.com/yIm-M5-BpSDdTEIJRt5D6xphizhIdozXjqSITgK4phWq7MmAU3qE7Nw7POGCiPGyhtJ3ZFP8iJ29TFl-RLcGBWX5qI4-ZcnCPcsY4zI=s120",
+    link: "https://otherside.xyz/",
+    tittle: "ENTER the OTHERSIDE",
+    description:
+      "Otherside is a gamified, interoperable metaverse currently under development. The game blends mechanics from massively multiplayer online role playing games (MMORPGs) and web3-enabled virtual worlds. Think of it as a metaRPG where the players own the world, your NFTs can become playable characters, and thousands can play together in real time.",
+      whitepaper: "https://otherside.xyz/litepaper",
+      sites: [
+        "",
+      ],
+      socials: [
+        { twitter: "https://twitter.com/OthersideMeta" },
+        { discord: "https://discord.gg/the-otherside" },
+        { telegram: "" },
+        { reddit: "https://www.reddit.com/r/OtherSide/" },
+        { instagram: "" },
+        { youtube: "https://www.youtube.com/channel/UCB6R9NAjkgxQi_QEkc4O25Q" },
+        { facebook: "" },
+      ],
+  },
+];
+export const etherScan = "https://etherscan.io/address/";
+export const poligonScan = "https://polygonscan.com/address/";
